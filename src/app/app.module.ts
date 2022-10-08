6fb5c4908454c93aeed031563780114f841aaa10
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-08 22:55:58 +0530

    aboutus program added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { GalleryComponent } from './gallery/gallery.component';
     NavbarComponent,
     HomeComponent,
     ContactComponent,
-    GalleryComponent
+    GalleryComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
